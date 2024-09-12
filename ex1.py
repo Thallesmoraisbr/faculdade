27494ed7e0831913b9eb8afe02e0b7ccfa6ec89e
thalles lima <thalleslimabr@gmail.com>
2024-09-11 21:55:25 -0300

    first commit

---
+++ ex1.py
@@ -0,0 +1,9 @@
+num1 = float(input("Informe o valor 1:"))
+num2 = float(input("Informe o valor 2:"))
+num3 = float(input("Informe o valor 3:"))
+num4 = float(input("Informe o valor 4:"))
+
+soma = (num1 + num2 + num3 + num4 )
+media = soma / 4
+
+print("Média é:"+str(media))
